f3e62189872573da41f8afb4eed84afbf6f751b4
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-04 14:47:24 +0200

    Login and register basic design added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h3>Username</h3>
-            <input type={"text"}/>
-            <h3>Password</h3>
-            <input type={"text"}/>
-            <button>Login</button>
-            <a href="/register">
-                <button>Register</button>
-            </a>
+  return (
+    <div className="form-container">
+      <h2>Welcome back!</h2>
+      <form>
+        <div className="form-input">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
         </div>
-    )
-}
+        <div className="form-input">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <button className="login-btn">Login</button>
+      </form>
+      <div className="register-link">
+        <p>Don't have an account?</p>
+        <Link to="/register">Register here</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,15 +2,20 @@ import React from "react";
 
 const Register = () => {
     return (
-        <div>
-            <h3>Username</h3>
-            <input type={"text"}/>
-            <h3>Password</h3>
-            <input type={"text"}/>
-            <h3>Confirm password</h3>
-            <input type={"text"}/>
-            <button>Register</button>
+        <div className="form-container">
+      <h2>Welcome to us!</h2>
+      <form>
+        <div className="form-input">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
         </div>
+        <div className="form-input">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <button className="login-btn">Register</button>
+      </form>
+      </div>
     )
 }
 
