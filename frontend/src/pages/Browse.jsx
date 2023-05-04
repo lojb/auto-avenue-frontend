2afed8fbdf013327e36b1603159da637f4ff5613
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-04 15:29:20 +0200

    Browse page basic styling added

--- frontend/src/pages/Browse.jsx
+++ frontend/src/pages/Browse.jsx
@@ -1,36 +1,43 @@
 import React from "react";
 
 const Browse = () => {
-    return (
-        <div>
-            <div id={"filter"}>
-                <h2>Filter menu</h2>
-                <h3>filter by year</h3>
-                <p>From</p>
-                <input type={"number"}/>
-                <p>To</p>
-                <input type={"number"}/>
-                <h3>filter by make</h3>
-                <select>
-                    <option>make1</option>
-                    <option>make2</option>
-                    <option>make3</option>
-                </select>
-                <h3>filter by model</h3>
-                <input type={"text"}/>
-                <button>Apply filters</button>
-            </div>
-            <div>
-                <ul>
-                    <li>car1</li>
-                    <li>car2</li>
-                    <li>car3</li>
-                    <li>car4</li>
-                    <li>car5</li>
-                </ul>
-            </div>
+  return (
+    <div className="container">
+      <div id="filter">
+        <h2>Filter menu</h2>
+        <h3>Filter by year</h3>
+        <div className="filter-input-group">
+          <p>From</p>
+          <input type="number" />
+          <p>To</p>
+          <input type="number" />
         </div>
-    )
-}
+        <h3>Filter by make</h3>
+        <div className="filter-input-group">
+          <select>
+            <option>--</option>
+            <option>make1</option>
+            <option>make2</option>
+            <option>make3</option>
+          </select>
+        </div>
+        <h3>Filter by model</h3>
+        <div className="filter-input-group">
+          <input type="text" />
+        </div>
+        <button className="filter-button">Apply filters</button>
+      </div>
+      <div>
+        <ul className="car-list">
+          <li>car1</li>
+          <li>car2</li>
+          <li>car3</li>
+          <li>car4</li>
+          <li>car5</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Browse
+export default Browse;
