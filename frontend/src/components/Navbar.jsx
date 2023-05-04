abb00d188d52179f7f30c5bdfe10a79bc454d2ef
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-04 11:39:40 +0200

    Basic navbar created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Logo } from '../assets';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar__logo-container">
+      <div className="navbar__logo-background">
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="navbar__logo" />
+        </Link>
+        </div>
+      </div>
+      <div className="navbar__links-container">
+        <Link to="/contact" className="navbar__link">Contact</Link>
+        <Link to="/about" className="navbar__link">About</Link>
+        <Link to="/login" className="navbar__button">Login</Link>
+        <Link to="/signup" className="navbar__button navbar__button--signup">Sign Up</Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
