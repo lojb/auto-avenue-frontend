3e82e92415c85cd074d5976763968464fc330a0e
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-17 01:16:00 +0200

    Sample car picture exported from assets

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -3,5 +3,6 @@ import phone from "./phone.png";
 import location from "./location.png";
 import facebookLogo from "./facebookicon.png";
 import instagramLogo from "./instagram.png";
+import carPicture from "./carpicture.png";
 
-export { Logo, phone, location, facebookLogo, instagramLogo };
+export { Logo, phone, location, facebookLogo, instagramLogo, carPicture };
