20025a756d2def33b3db497f94e6eb842f514d75
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-17 01:17:18 +0200

    Link added to Create route from Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Logo } from '../assets';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Logo } from "../assets";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__logo-container">
-      <div className="navbar__logo-background">
-        <Link to="/">
-          <img src={Logo} alt="Logo" className="navbar__logo" />
-        </Link>
+        <div className="navbar__logo-background">
+          <Link to="/">
+            <img src={Logo} alt="Logo" className="navbar__logo" />
+          </Link>
         </div>
       </div>
       <div className="navbar__links-container">
-        <Link to="/contact" className="navbar__link">Contact</Link>
-        <Link to="/about" className="navbar__link">About</Link>
-        <Link to="/login" className="navbar__button">Login</Link>
-        <Link to="/register" className="navbar__button navbar__button--signup">Sign Up</Link>
+        <Link to="/create" className="navbar__link">
+          Create
+        </Link>
+        <Link to="/contact" className="navbar__link">
+          Contact
+        </Link>
+        <Link to="/about" className="navbar__link">
+          About
+        </Link>
+        <Link to="/login" className="navbar__button">
+          Login
+        </Link>
+        <Link to="/register" className="navbar__button navbar__button--signup">
+          Sign Up
+        </Link>
       </div>
     </nav>
   );
