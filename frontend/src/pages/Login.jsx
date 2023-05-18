5f9ef435a98fce715a883d4ff904860c08207788
lojb <balazsloj02@gmail.com>
2023-05-18 17:14:03 +0200

    Implement basic login feature to store logged in user

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,30 @@
-import { Link } from "react-router-dom";
+import {Link, redirect, useNavigate} from "react-router-dom";
+import {useState} from "react";
+import {useSnapshot} from "valtio";
+import {state} from "../store";
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const snap = useSnapshot(state);
+    const navigate = useNavigate();
+    const handleLogin = () => {
+        state.username = username;
+        state.isUser = true;
+        navigate("/");
+    }
   return (
     <div className="form-container">
       <h2>Welcome back!</h2>
       <form className="login-register-form">
         <div className="form-input">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input type="text" id="username" onChange={(e) => setUsername(e.target.value)}/>
         </div>
         <div className="form-input">
           <label htmlFor="password">Password</label>
           <input type="password" id="password" />
         </div>
-        <button className="login-btn">Login</button>
+        <button className="login-btn" onClick={handleLogin}>Login</button>
       </form>
       <div className="register-link">
         <p>Don't have an account?</p>
