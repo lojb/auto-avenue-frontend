899eb0df43433763db3407c518bb57257907041f
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-18 10:12:54 +0200

    Welcome page created as single page layout

---
+++ frontend/src/pages/Welcome.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Feedback from "../components/Feedback";
+import { background } from "../assets";
+
+const Welcome = () => {
+  return (
+    <>
+      <div className="relative z-0 bg-white">
+        <div
+          className="bg-cover bg-center bg-no-repeat min-h-screen"
+          style={{ backgroundImage: `url(${background})` }}
+        >
+          <Hero />
+        </div>
+        <About />
+
+        <Contact />
+        <Feedback />
+      </div>
+    </>
+  );
+};
+
+export default Welcome;
