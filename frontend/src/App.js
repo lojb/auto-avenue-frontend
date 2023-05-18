3ddbfff0c1f67d6c8f4fed41a7ba99600911362a
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-18 10:14:03 +0200

    Neccessary dependencies added and basic styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,35 +7,33 @@ import {
 } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Browse from "./pages/Browse";
 import Create from "./pages/Create";
-import AboutUs from "./pages/AboutUs";
+
 import AdminPage from "./pages/AdminPage";
-import Contact from "./pages/Contact";
+
 import AdvertDetails from "./pages/AdvertDetails";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/browse" element={<Browse />}></Route>
-          <Route path="/create" element={<Create />}></Route>
-          <Route path="/about" element={<AboutUs />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/admin" element={<AdminPage />}></Route>
-          <Route path="/browse/:id" element={<AdvertDetails />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/browse" element={<Browse />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+
+        <Route path="/admin" element={<AdminPage />}></Route>
+        <Route path="/browse/:id" element={<AdvertDetails />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,8 +1,27 @@
-import Logo from "./autoAvenueLogo.png";
+import Logo from "./auto-avenue-low-resolution-color-logo.png";
 import phone from "./phone.png";
 import location from "./location.png";
 import facebookLogo from "./facebookicon.png";
 import instagramLogo from "./instagram.png";
 import carPicture from "./carpicture.png";
+import background from "./background.jpg";
+import quality from "./high-quality.png";
+import satisfaction from "./satisfaction.png";
+import verify from "./authenticity.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
 
-export { Logo, phone, location, facebookLogo, instagramLogo, carPicture };
+export {
+  Logo,
+  phone,
+  location,
+  facebookLogo,
+  instagramLogo,
+  carPicture,
+  background,
+  quality,
+  satisfaction,
+  verify,
+  menu,
+  close,
+};
