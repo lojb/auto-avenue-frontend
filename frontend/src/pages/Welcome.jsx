5a5ef9e53f9d0926f3ea36ea387544adc7073508
SzimkoBalazs <ubeats1995@gmail.com>
2023-05-31 10:47:01 +0200

    useSnapshot removed

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,26 +1,11 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Feedback from "../components/Feedback";
 import { background } from "../assets";
-import {useSnapshot} from "valtio";
-import {state} from "../store";
 
 const Welcome = () => {
-  const snap = useSnapshot(state);
-
-  useEffect(() => {
-    fetch(`http://localhost:8080/users/username/${snap.username}`)
-        .then((response) => response.json())
-        .then((data) => {
-          state.userId = data.id;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-  }, []);
-
   return (
     <>
       <div className="relative z-0 bg-white">
