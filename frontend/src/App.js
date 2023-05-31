801bc4bc33d12d76a5054406a9df888d0baabfd8
lojb <balazsloj02@gmail.com>
2023-05-31 15:53:41 +0200

    Add redirect to main if the user is not ADMIN

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import {
-  BrowserRouter,
-  Routes,
-  Router,
-  Route,
-  Navigate,
+    BrowserRouter,
+    Routes,
+    Router,
+    Route,
+    Navigate,
 } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -17,31 +17,35 @@ import AdminPage from "./pages/AdminPage";
 
 import AdvertDetails from "./pages/AdvertDetails";
 import Welcome from "./pages/Welcome";
+import Wishlist from "./pages/Wishlist";
 
 function App() {
-  const { user } = useAuthContext();
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Welcome />}></Route>
-        <Route
-          path="/login"
-          element={!user ? <Login /> : <Navigate to="/" />}
-        ></Route>
-        <Route
-          path="/register"
-          element={!user ? <Register /> : <Navigate to="/" />}
-        ></Route>
-        <Route path="/browse" element={<Browse />}></Route>
-        <Route path="/create" element={<Create />}></Route>
+    const {user} = useAuthContext();
 
-        <Route path="/admin" element={<AdminPage />}></Route>
-        <Route path="/browse/:id" element={<AdvertDetails />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Welcome/>}></Route>
+                <Route
+                    path="/login"
+                    element={!user ? <Login/> : <Navigate to="/"/>}
+                ></Route>
+                <Route
+                    path="/register"
+                    element={!user ? <Register/> : <Navigate to="/"/>}
+                ></Route>
+                <Route path="/browse" element={<Browse/>}></Route>
+                <Route path="/create" element={<Create/>}></Route>
+                <Route path="/wishlist" element={<Wishlist/>}></Route>
+                <Route path="/admin"
+                       element={(user && user.role === "ADMIN") ? <AdminPage/> : <Navigate to="/"/>}
+                ></Route>
+                <Route path="/browse/:id" element={<AdvertDetails/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
