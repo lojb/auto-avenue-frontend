1f64c76e5dec4f8afb64abf3378cbab3f98209ae
SzimkoBalazs <ubeats1995@gmail.com>
2023-06-02 20:18:54 +0200

    Picture to be shown in myadverts

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,143 +1,142 @@
-import React, {useState} from "react";
-import {Link} from "react-router-dom";
-import {styles} from "../styles";
-import {Logo, menu, close, profile} from "../assets";
-import {navLinks} from "../constants";
-import {useLogout} from "../hooks/useLogout";
-import {useAuthContext} from "../hooks/useAuthContext";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { Logo, menu, close, profile } from "../assets";
+import { navLinks } from "../constants";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
-    const {logout} = useLogout();
-    const {user} = useAuthContext();
-    const [active, setActive] = useState("");
-    const [toggle, setToggle] = useState(false);
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
 
-    const handleClick = () => {
-        logout();
-    };
+  const handleClick = () => {
+    logout();
+  };
 
-    return (
-        <nav
-            className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-slate-300`}
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-slate-300`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
         >
-            <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
-                <Link
-                    to="/"
-                    className="flex items-center gap-2"
-                    onClick={() => {
-                        setActive("");
-                        window.scrollTo(0, 0);
-                    }}
-                >
-                    <img
-                        src={Logo}
-                        alt="logo"
-                        className="w-[100px] h-[50px] object-contain"
-                    />
-                </Link>
-                <ul className="list-none hidden sm:flex flex-row gap-10">
-                    {navLinks.map((link) => (
-                        <li
-                            key={link.id}
-                            className={`${
-                                active === link.title ? "text-white" : "text-secondary"
-                            }
+          <img
+            src={Logo}
+            alt="logo"
+            className="w-[100px] h-[50px] object-contain rounded"
+          />
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              }
                            hover:text-white text-[18px] font-medium cursor-pointer`}
-                            onClick={() => setActive(link.title)}
-                        >
-                            <a href={`#${link.id}`}>{link.title}</a>
-                        </li>
-                    ))}
-                </ul>
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
 
-                <div className="sm:hidden flex flex-1 justify-end items-center">
-                    <img
-                        src={toggle ? close : menu}
-                        alt="menu"
-                        className="w-[28px] h-[28px] object-contain cursor-pointer"
-                        onClick={() => setToggle(!toggle)}
-                    />
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
 
-                    <div
-                        className={`${!toggle ? "hidden" : "flex"} 
+          <div
+            className={`${!toggle ? "hidden" : "flex"} 
                                   p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-                    >
-                        <ul className="list-none flex justify-end items-start flex-col gap-4">
-                            {navLinks.map((link) => (
-                                <li
-                                    key={link.id}
-                                    className={`${
-                                        active === link.title ? "text-white" : "text-secondary"
-                                    }
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  }
                           font-poppins font-medium cursor-pointer text-[16px]`}
-                                    onClick={() => {
-                                        setToggle(!toggle);
-                                        setActive(link.title);
-                                    }}
-                                >
-                                    <a href={`#${link.id}`}>{link.title}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-                {user && (
-                    <div>
-                        <Link
-                            to="/create"
-                            className="px-4 py-2 rounded-md bg-secondary text-black font-medium hover:bg-secondary-dark focus:outline-none focus:ring-2 focus:ring-secondary"
-                        >
-                            Create
-                        </Link>
-                    </div>
-                )}
-                {user && user.role === "ADMIN" && (
-                    <div>
-                        <Link
-                            to="/admin"
-                            className="px-4 py-2 rounded-md bg-secondary text-black font-medium hover:bg-secondary-dark focus:outline-none focus:ring-2 focus:ring-secondary"
-                        >
-                            AdminPanel
-                        </Link>
-                    </div>
-                )}
-                {user && (
-                    <div className="text-white flex items-center gap-2">
-                        <Link to={`/profilepage/${user.userId}`}>
-                            <div className="flex items-center">
-                                <span className="mr-2">{user.username}</span>
-                                <img className="w-8 h-8" src={profile} alt="profile" />
-                            </div>
-                        </Link>
-                        <Link to={"/"}>
-                            <button
-                                className="ml-4 px-4 py-2 rounded-md bg-red-600 text-white font-medium hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
-                                onClick={handleClick}
-                            >
-                                Log out
-                            </button>
-                        </Link>
-                    </div>
-                )}
-                {!user && (
-                    <div>
-                        <Link
-                            to="/login"
-                            className="px-4 py-2 rounded-md bg-secondary text-black font-medium hover:bg-secondary-dark focus:outline-none focus:ring-2 focus:ring-secondary"
-                        >
-                            Login
-                        </Link>
-                        <Link to="/register">
-                            <button
-                                className="px-4 py-2 rounded-md bg-primary text-black font-medium hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-primary">
-                                Signup
-                            </button>
-                        </Link>
-                    </div>
-                )}
-            </div>
-        </nav>
-    );
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        {user && (
+          <div>
+            <Link
+              to="/create"
+              className="px-4 py-2 rounded-md bg-secondary text-black font-medium hover:bg-secondary-dark focus:outline-none focus:ring-2 focus:ring-secondary"
+            >
+              Create
+            </Link>
+          </div>
+        )}
+        {user && user.role === "ADMIN" && (
+          <div>
+            <Link
+              to="/admin"
+              className="px-4 py-2 rounded-md bg-secondary text-black font-medium hover:bg-secondary-dark focus:outline-none focus:ring-2 focus:ring-secondary"
+            >
+              AdminPanel
+            </Link>
+          </div>
+        )}
+        {user && (
+          <div className="text-white flex items-center gap-2">
+            <Link to={`/profilepage/${user.userId}`}>
+              <div className="flex items-center">
+                <span className="mr-2">{user.username}</span>
+                <img className="w-8 h-8" src={profile} alt="profile" />
+              </div>
+            </Link>
+            <Link to={"/"}>
+              <button
+                className="ml-4 px-4 py-2 rounded-md bg-red-600 text-white font-medium hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
+                onClick={handleClick}
+              >
+                Log out
+              </button>
+            </Link>
+          </div>
+        )}
+        {!user && (
+          <div>
+            <Link
+              to="/login"
+              className="px-4 py-2 rounded-md bg-secondary text-black font-medium hover:bg-secondary-dark focus:outline-none focus:ring-2 focus:ring-secondary"
+            >
+              Login
+            </Link>
+            <Link to="/register">
+              <button className="px-4 py-2 rounded-md bg-primary text-black font-medium hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-primary">
+                Signup
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- frontend/src/components/UserAdverts.jsx
+++ frontend/src/components/UserAdverts.jsx
@@ -1,238 +1,231 @@
-import React, {useState, useEffect} from "react";
-import {useAuthContext} from "../hooks/useAuthContext";
-import {carPicture, bin, edit} from "../assets";
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { carPicture, bin, edit } from "../assets";
+import { Link } from "react-router-dom";
 
 const UserAdverts = () => {
-    const {user} = useAuthContext();
-    const [adverts, setAdverts] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isUpdating, setIsUpdating] = useState(false);
-    const [editedAdvert, setEditedAdvert] = useState(null);
-    const [editedData, setEditedData] = useState({
-        manufacturer: "",
-        model: "",
-        year: "",
-        price: "",
+  const { user } = useAuthContext();
+  const [adverts, setAdverts] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [editedAdvert, setEditedAdvert] = useState(null);
+  const [editedData, setEditedData] = useState({
+    manufacturer: "",
+    model: "",
+    year: "",
+    price: "",
+  });
+
+  useEffect(() => {
+    const fetchUserAdverts = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/adverts/user/${user.userId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          }
+        );
+        const data = await response.json();
+        setAdverts(data);
+      } catch (error) {
+        console.log("Error fetching user adverts:", error);
+      }
+    };
+
+    fetchUserAdverts();
+  }, [user.token]);
+
+  useEffect(() => {
+    if (editedAdvert) {
+      setEditedData(editedAdvert);
+    }
+  }, [editedAdvert]);
+
+  const deleteAdvert = async (advertId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/adverts/${advertId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        setAdverts((prevAdverts) =>
+          prevAdverts.filter((advert) => advert.id !== advertId)
+        );
+      } else {
+        console.log("Failed to delete advert");
+      }
+    } catch (error) {
+      console.log("Error deleting advert:", error);
+    }
+  };
+
+  const openEditModal = (advertId) => {
+    const advert = adverts.find((item) => item.id === advertId);
+    setEditedAdvert(advert);
+    setEditedData(advert);
+    setIsModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setIsModalOpen(false);
+    setEditedData({
+      id: "",
+      manufacturer: "",
+      model: "",
+      year: "",
+      price: "",
     });
+  };
 
-    useEffect(() => {
-        const fetchUserAdverts = async () => {
-            try {
-                const response = await fetch(
-                    `http://localhost:8080/adverts/user/${user.userId}`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${user.token}`,
-                        },
-                    }
-                );
-                const data = await response.json();
-                setAdverts(data);
-            } catch (error) {
-                console.log("Error fetching user adverts:", error);
-            }
-        };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditedData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-        fetchUserAdverts();
-    }, [user.token]);
-
-    useEffect(() => {
-        if (editedAdvert) {
-            setEditedData(editedAdvert);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsUpdating(true);
+    try {
+      const response = await fetch(
+        `http://localhost:8080/adverts/${editedAdvert.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.token}`,
+          },
+          body: JSON.stringify(editedData),
         }
-    }, [editedAdvert]);
+      );
 
-    const deleteAdvert = async (advertId) => {
-        try {
-            const response = await fetch(
-                `http://localhost:8080/adverts/${advertId}`,
-                {
-                    method: "DELETE",
-                    headers: {
-                        Authorization: `Bearer ${user.token}`,
-                    },
-                }
-            );
+      if (response.ok) {
+        setAdverts((prevAdverts) =>
+          prevAdverts.map((advert) =>
+            advert.id === editedData.id ? editedData : advert
+          )
+        );
+        closeEditModal();
+      } else {
+        console.log("Failed to update advert");
+      }
+    } catch (error) {
+      console.log("Error updating advert:", error);
+    }
+    setIsUpdating(false);
+  };
 
-            if (response.ok) {
-                setAdverts((prevAdverts) =>
-                    prevAdverts.filter((advert) => advert.id !== advertId)
-                );
-            } else {
-                console.log("Failed to delete advert");
-            }
-        } catch (error) {
-            console.log("Error deleting advert:", error);
-        }
-    };
-
-    const openEditModal = (advertId) => {
-        const advert = adverts.find((item) => item.id === advertId);
-        setEditedAdvert(advert);
-        setEditedData(advert);
-        setIsModalOpen(true);
-    };
-
-    const closeEditModal = () => {
-        setIsModalOpen(false);
-        setEditedData({
-            id: "",
-            manufacturer: "",
-            model: "",
-            year: "",
-            price: "",
-        });
-    };
-
-    const handleInputChange = (event) => {
-        const {name, value} = event.target;
-        setEditedData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setIsUpdating(true);
-        try {
-            const response = await fetch(
-                `http://localhost:8080/adverts/${editedAdvert.id}`,
-                {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${user.token}`,
-                    },
-                    body: JSON.stringify(editedData),
-                }
-            );
-
-            if (response.ok) {
-                setAdverts((prevAdverts) =>
-                    prevAdverts.map((advert) =>
-                        advert.id === editedData.id ? editedData : advert
-                    )
-                );
-                closeEditModal();
-            } else {
-                console.log("Failed to update advert");
-            }
-        } catch (error) {
-            console.log("Error updating advert:", error);
-        }
-        setIsUpdating(false);
-    };
-
-    return (
-        <div className="grid grid-cols-2 gap-4">
-            {adverts && adverts.length === 0 &&
-                <div>
-                    <h1>You have no adverts yet</h1>
-                    <h1>To create a new advert, click on the button below</h1>
-                    <Link to={"/create"}>
-                        <button
-                            className="px-4 py-2 rounded-md bg-blue-600 text-white font-medium hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
-                        >
-                            Create
-                        </button>
-                    </Link>
-                </div>
-            }
-            {adverts && adverts.map((advert) => (
-                <div key={advert.id} className="bg-white rounded-lg p-4 shadow">
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {adverts && adverts.length === 0 && (
+        <div>
+          <h1>You have no adverts yet</h1>
+          <h1>To create a new advert, click on the button below</h1>
+          <Link to={"/create"}>
+            <button className="px-4 py-2 rounded-md bg-blue-600 text-white font-medium hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50">
+              Create
+            </button>
+          </Link>
+        </div>
+      )}
+      {adverts &&
+        adverts.map((advert) => (
+          <div key={advert.id} className="bg-white rounded-lg p-4 shadow">
             <img
-                src={carPicture}
-                alt="Advert Image"
-                className="w-full h-40 object-cover mb-4 rounded-lg"
+              src={advert.imageUrl}
+              alt="Advert Image"
+              className="w-full h-60 object-cover mb-4 rounded-lg"
             />
             <h3 className="text-xl font-medium">
-                {advert.manufacturer} {advert.model}
+              {advert.manufacturer} {advert.model}
             </h3>
             <p className="text-gray-500">Year: {advert.year}</p>
             <p className="text-gray-500">Price: {advert.price}</p>
             <div className="flex justify-between mt-4">
-                <img
-                    src={edit}
-                    alt="Update Icon"
-                    className="w-6 h-6 cursor-pointer"
-                    onClick={() => openEditModal(advert.id)}
-                />
-                <img
-                    src={bin}
-                    alt="Delete Icon"
-                    className="w-6 h-6 cursor-pointer"
-                    onClick={() => deleteAdvert(advert.id)}
-                />
+              <img
+                src={edit}
+                alt="Update Icon"
+                className="w-6 h-6 cursor-pointer"
+                onClick={() => openEditModal(advert.id)}
+              />
+              <img
+                src={bin}
+                alt="Delete Icon"
+                className="w-6 h-6 cursor-pointer"
+                onClick={() => deleteAdvert(advert.id)}
+              />
             </div>
-        </div>
-    )
-)
-}
+          </div>
+        ))}
 
-    {/* Edit Advert Modal */
-    }
-    {
-        isModalOpen && (
-            <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-                <div className="bg-white rounded-lg p-4 shadow">
-                    <form className="space-y-4" onSubmit={handleSubmit}>
-                        {/* Form inputs for editing advert details */}
-                        <label className="text-sm font-medium">Price:</label>
-                        <input
-                            type="text"
-                            name="price"
-                            value={editedData.price}
-                            onChange={handleInputChange}
-                            className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                        />
-                        <label className="text-sm font-medium">Manufacturer:</label>
-                        <input
-                            type="text"
-                            name="manufacturer"
-                            value={editedData.manufacturer}
-                            onChange={handleInputChange}
-                            className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                        />
-                        <label className="text-sm font-medium">Model:</label>
-                        <input
-                            type="text"
-                            name="model"
-                            value={editedData.model}
-                            onChange={handleInputChange}
-                            className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                        />
-                        <label className="text-sm font-medium">Year:</label>
-                        <input
-                            type="text"
-                            name="year"
-                            value={editedData.year}
-                            onChange={handleInputChange}
-                            className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                        />
-                        <div className="flex justify-end space-x-2">
-                            <button
-                                type="submit"
-                                className="px-4 py-2 text-sm font-medium text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
-                            >
-                                Update
-                            </button>
-                            <button
-                                onClick={closeEditModal}
-                                className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none focus:bg-gray-300"
-                            >
-                                Cancel
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
-</div>
-)
-    ;
+      {/* Edit Advert Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg p-4 shadow">
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              {/* Form inputs for editing advert details */}
+              <label className="text-sm font-medium">Price:</label>
+              <input
+                type="text"
+                name="price"
+                value={editedData.price}
+                onChange={handleInputChange}
+                className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              />
+              <label className="text-sm font-medium">Manufacturer:</label>
+              <input
+                type="text"
+                name="manufacturer"
+                value={editedData.manufacturer}
+                onChange={handleInputChange}
+                className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              />
+              <label className="text-sm font-medium">Model:</label>
+              <input
+                type="text"
+                name="model"
+                value={editedData.model}
+                onChange={handleInputChange}
+                className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              />
+              <label className="text-sm font-medium">Year:</label>
+              <input
+                type="text"
+                name="year"
+                value={editedData.year}
+                onChange={handleInputChange}
+                className="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              />
+              <div className="flex justify-end space-x-2">
+                <button
+                  type="submit"
+                  className="px-4 py-2 text-sm font-medium text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+                >
+                  Update
+                </button>
+                <button
+                  onClick={closeEditModal}
+                  className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none focus:bg-gray-300"
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserAdverts;
